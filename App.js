d0b274a49dce8c5c1b1d5b96f82cd25026022a25
gracie2 <stevenmanangu360@gmail.com>
2022-04-04 20:36:42 +0300

    finished setting up redux

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
-// setup redux
 
 export default function App() {
   return (
 
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets Build Alpha AI</Text>
         <StatusBar style="auto" />
