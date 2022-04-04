dc2c2686b6f425eb8263c391b2c1edf7aab1e0d2
gracie2 <stevenmanangu360@gmail.com>
2022-04-04 23:13:27 +0300

    added tailwind css

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native'
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={tw`text-red-500`}>I am the HomeScreen</Text>
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight
+    }
+})
