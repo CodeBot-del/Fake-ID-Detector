48398219043cc3ff018a583f8c4ee37dc3755f49
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 23:34:19 +0300

    implemented image picker

--- screens/FaceScreen.js
+++ screens/FaceScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, TouchableOpacity, Image, Platform  } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TouchableOpacity, Image, Platform } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
@@ -7,40 +7,60 @@ import * as ImagePicker from 'expo-image-picker';
 
 
 const FaceScreen = () => {
+
+    const [image, setImage] = useState(null);
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
     return (
         <SafeAreaView style={[styles.container, tw`h-full`]}>
-            <View style={[tw`p-6 items-center pt-10`,{height: 500}]}>
+            <View style={[tw`p-6 items-center pt-10`, { height: 500 }]}>
                 <Text style={tw`p-2 font-semibold text-lg`}>Facial Scan</Text>
                 <TouchableOpacity
-                    style={[tw`bg-gray-400 items-center justify-center mb-5`, {height: 200, width: 300, borderRadius: 10}]}
-                >   
-                    <Icon 
-                            style={tw`p-2 w-20 `}
-                            name="upload"
-                            color="white"
-                            type="entypo"
-                            size={50}
-                        />
+                    onPress={pickImage}
+                    style={[tw`bg-gray-400 items-center justify-center mb-5`, { height: 200, width: 300, borderRadius: 10 }]}
+                >
+                    <Icon
+                        style={tw`p-2 w-20 `}
+                        name="upload"
+                        color="white"
+                        type="entypo"
+                        size={50}
+                    />
                     <Text style={tw`text-white text-center`}>Upload ID Card Image</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    style={[tw`bg-black items-center justify-center`, {height: 60, width: 100, borderRadius: 15}]}
+                    style={[tw`bg-black items-center justify-center`, { height: 60, width: 100, borderRadius: 15 }]}
                 >
                     <Text style={tw`text-white text-center text-lg font-semibold `}>Scan</Text>
                 </TouchableOpacity>
+                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
             </View>
             <View style={[tw`mr-2 mt-6 p-4`, styles.cameraContainer]}>
                 <TouchableOpacity
-                    style={[tw`bg-black items-center justify-center rounded-full mt-8`, {height: 60, width: 60}]}
+                    style={[tw`bg-black items-center justify-center rounded-full mt-8`, { height: 60, width: 60 }]}
                 >
-                        <Icon 
-                            style={tw`p-2 w-20 `}
-                            name="camera"
-                            color="white"
-                            type="antdesign"
-                            size={40}
-                        />
+                    <Icon
+                        style={tw`p-2 w-20 `}
+                        name="camera"
+                        color="white"
+                        type="antdesign"
+                        size={40}
+                    />
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -54,7 +74,7 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: StatusBar.currentHeight,
     },
-    cameraContainer:{
+    cameraContainer: {
         alignItems: 'flex-end',
 
     }
