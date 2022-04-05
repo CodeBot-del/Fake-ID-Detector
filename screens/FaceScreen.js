1eadb65eab72d88897db6f44455a1876be86dc04
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 22:40:49 +0300

    done with face scan UI

--- screens/FaceScreen.js
+++ screens/FaceScreen.js
@@ -10,27 +10,27 @@ const FaceScreen = () => {
         <SafeAreaView style={[styles.container, tw`h-full`]}>
             <View style={[tw`p-6 items-center pt-20`,{height: 500}]}>
                 <TouchableOpacity
-                    style={[tw`bg-red-500 items-center justify-center mb-5 rounded-full`, {height: 100, width: 300}]}
+                    style={[tw`bg-gray-400 items-center justify-center mb-5`, {height: 200, width: 300}]}
                 >   
                     <Icon 
                             style={tw`p-2 w-20 `}
                             name="upload"
                             color="white"
                             type="entypo"
-                            size={40}
+                            size={50}
                         />
                     <Text style={tw`text-white text-center`}>Upload ID Card Image</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    style={[tw`bg-blue-500 items-center justify-center`, {height: 80, width: 150}]}
+                    style={[tw`bg-black items-center justify-center`, {height: 60, width: 100, borderRadius: 15}]}
                 >
-                    <Text style={tw`text-white text-center`}>Scan</Text>
+                    <Text style={tw`text-white text-center text-lg font-semibold `}>Scan</Text>
                 </TouchableOpacity>
             </View>
             <View style={[tw`mr-2 mt-6 p-4`, styles.cameraContainer]}>
                 <TouchableOpacity
-                    style={[tw`bg-gray-700 items-center justify-center rounded-full mt-8`, {height: 60, width: 60}]}
+                    style={[tw`bg-black items-center justify-center rounded-full mt-8`, {height: 60, width: 60}]}
                 >
                         <Icon 
                             style={tw`p-2 w-20 `}
