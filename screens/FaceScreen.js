cad233a8781415e3147b611012c78fee0d0c3129
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 22:03:13 +0300

    added touchable opacity

--- screens/FaceScreen.js
+++ screens/FaceScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
@@ -10,24 +10,24 @@ const FaceScreen = () => {
         <SafeAreaView style={[styles.container, tw`h-full`]}>
             <Text>Here is the facial stuff...</Text>
             <View style={[tw`p-6 items-center pt-8 bg-gray-500`,{height: 500}]}>
-                <View
+                <TouchableOpacity
                     style={[tw`bg-red-500 items-center justify-center mb-5`, {height: 100, width: 300}]}
                 >
                     <Text style={tw`text-white text-center`}>Upload ID Card Image</Text>
-                </View>
+                </TouchableOpacity>
 
-                <View
+                <TouchableOpacity
                     style={[tw`bg-blue-500 items-center justify-center`, {height: 80, width: 150}]}
                 >
                     <Text style={tw`text-white text-center`}>Scan</Text>
-                </View>
+                </TouchableOpacity>
             </View>
             <View style={[tw`mr-4 mt-6 p-4`, styles.cameraContainer]}>
-                <View
+                <TouchableOpacity
                     style={[tw`bg-green-500 items-center justify-center`, {height: 80, width: 150}]}
                 >
                         <Text style={tw`text-white text-center`}>Camera</Text>
-                </View>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
