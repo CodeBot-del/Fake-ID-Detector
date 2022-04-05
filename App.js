3032ad6f968b6a7e90163ca6ca532690cfb9208f
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 14:36:34 +0300

    implementing stack navigator

--- App.js
+++ App.js
@@ -3,19 +3,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
 
     <Provider store={store}>
-      <SafeAreaProvider>
-      <HomeScreen/>
-      </SafeAreaProvider>
-      
+      <NavigationContainer>
+        <SafeAreaProvider>
+
+          <Stack.Navigator>
+            <Stack.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{
+              headerShown: false,
+            }}
+            />
+          </Stack.Navigator>
+
+          
+        </SafeAreaProvider>
+      </NavigationContainer>
+
+
     </Provider>
 
   );
