58792a934fdb9765bb66230b55b69421c0098362
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 09:56:49 +0300

    added github link

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,24 +3,14 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import { Icon } from 'react-native-elements';
-import ExternalLink from './ExternalLink';
+import * as WebBrowser from 'expo-web-browser';
 
-const bg = require('./bg.png')
-const data = [
-    {
-        id: "123",
-        title: 'Github',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
-        screen: 'GithubScreen',
-        link: 'https://github.com/CodeBot-del/Fake-ID-Detector',
-    },
-    {
-        id: "456",
-        title: 'Medium',
-        image: 'https://cdn-icons-png.flaticon.com/512/5968/5968854.png',
-        screen: 'QRCodeScreen',
-    }
-]
+const bg = require('./bg.png');
+const _handleOpenWithWebBrowser = () => {
+    WebBrowser.openBrowserAsync('https://github.com/CodeBot-del/Fake-ID-Detector');
+};
+
+
 const HomeScreen = () => {
 
     return (
@@ -41,25 +31,19 @@ const HomeScreen = () => {
 
             <View style={styles.container2}>
                 <ImageBackground source={bg} style={styles.image}>
-                    <View style={tw`p-5`}><Text style={tw`font-bold text-lg text-gray-700`}>Donate or contribute to the project</Text></View>
-                    <FlatList
-                        data={data}
-                        keyExtractor={(item) => item.id}
-                        horizontal
-                        renderItem={({ item }) => (
-                            <TouchableOpacity
-                                style={tw`p-6 pl-4 pb-8 pt-4 m-2 ml-12`}
-                            >
-                                <View>
-                                    <Image
-                                        style={{ width: 60, height: 60, }}
-                                        source={{ uri: item.image }}
-                                    />
-                                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                                </View>
-                            </TouchableOpacity>
-                        )}
-                    />
+                    <View style={tw`p-5 items-center`}><Text style={tw`font-bold text-sm text-gray-700`}>Donate or contribute to the project</Text></View>
+                    <View style={tw`items-center`}>
+                        <TouchableOpacity 
+                        style={tw`p-6`}
+                        onPress={_handleOpenWithWebBrowser}
+                        >
+                            <Image
+                                style={{ width: 60, height: 60, }}
+                                source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png' }}
+                            />
+                            <Text style={tw`mt-2 text-lg font-semibold pl-2`}>Github</Text>
+                        </TouchableOpacity>
+                    </View>
                     <View style={tw`p-5 items-center`}><Text style={tw`font-bold text-lg text-gray-700`}>@Alpha AI</Text></View>
 
                 </ImageBackground>
@@ -69,6 +53,8 @@ const HomeScreen = () => {
         </SafeAreaView>
     )
 }
+
+
 
 export default HomeScreen
 
@@ -84,8 +70,8 @@ const styles = StyleSheet.create({
     },
     container2: {
         flex: 1,
-        flexDirection: 'column',
+        
     },
-    
+
 
 })
