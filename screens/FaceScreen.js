c2b55edb570123a6c0829944a13985e6c41cf0be
gracie2 <stevenmanangu360@gmail.com>
2022-04-05 22:28:17 +0300

    working with icons

--- screens/FaceScreen.js
+++ screens/FaceScreen.js
@@ -8,11 +8,17 @@ import { Icon } from 'react-native-elements';
 const FaceScreen = () => {
     return (
         <SafeAreaView style={[styles.container, tw`h-full`]}>
-            <Text>Here is the facial stuff...</Text>
-            <View style={[tw`p-6 items-center pt-8 bg-gray-500`,{height: 500}]}>
+            <View style={[tw`p-6 items-center pt-20`,{height: 500}]}>
                 <TouchableOpacity
-                    style={[tw`bg-red-500 items-center justify-center mb-5`, {height: 100, width: 300}]}
-                >
+                    style={[tw`bg-red-500 items-center justify-center mb-5 rounded-full`, {height: 100, width: 300}]}
+                >   
+                    <Icon 
+                            style={tw`p-2 w-20 `}
+                            name="upload"
+                            color="white"
+                            type="entypo"
+                            size={40}
+                        />
                     <Text style={tw`text-white text-center`}>Upload ID Card Image</Text>
                 </TouchableOpacity>
 
@@ -22,11 +28,17 @@ const FaceScreen = () => {
                     <Text style={tw`text-white text-center`}>Scan</Text>
                 </TouchableOpacity>
             </View>
-            <View style={[tw`mr-4 mt-6 p-4`, styles.cameraContainer]}>
+            <View style={[tw`mr-2 mt-6 p-4`, styles.cameraContainer]}>
                 <TouchableOpacity
-                    style={[tw`bg-green-500 items-center justify-center`, {height: 80, width: 150}]}
+                    style={[tw`bg-gray-700 items-center justify-center rounded-full mt-8`, {height: 60, width: 60}]}
                 >
-                        <Text style={tw`text-white text-center`}>Camera</Text>
+                        <Icon 
+                            style={tw`p-2 w-20 `}
+                            name="camera"
+                            color="white"
+                            type="antdesign"
+                            size={40}
+                        />
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
