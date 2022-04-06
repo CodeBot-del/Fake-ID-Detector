b38d6de5c373270f7d0c268287d2a7a5e3bb98f3
gracie2 <stevenmanangu360@gmail.com>
2022-04-06 10:39:12 +0300

    getting serious with the camera

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,44 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity,StatusBar } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
 import { Camera } from 'expo-camera';
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
 
 const CameraScreen = () => {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={[styles.container,]}>
-      <Camera style={[styles.camera,]} type={type}>
-        <View>
-          <TouchableOpacity
-            
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+    return (
+        <View style={[styles.container,]}>
+            <Camera style={[styles.camera,]} type={type}>
+                <View style={[tw`bg-gray-500`, { width: '100%', height: 'auto' }]}>
+                    <View>
+                        <TouchableOpacity
+                            style={tw``}
+                            onPress={() => {
+                                setType(
+                                    type === Camera.Constants.Type.back
+                                        ? Camera.Constants.Type.front
+                                        : Camera.Constants.Type.back
+                                );
+                            }}>
+                            <Icon
+                                style={tw`p-2 w-20 `}
+                                name="retweet"
+                                color="white"
+                                type="entypo"
+                                size={30}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </Camera>
         </View>
-      </Camera>
-    </View>
-  );
+    );
 }
 
 export default CameraScreen
@@ -49,10 +59,10 @@ const styles = StyleSheet.create({
         height: '100%',
         marginTop: StatusBar.currentHeight,
 
-        
+
     },
-    camera:{
+    camera: {
         flex: 1,
         height: '100%',
     }
- }); 
+}); 
